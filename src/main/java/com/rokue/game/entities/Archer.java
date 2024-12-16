ee859ed9f3780d5446f3eba29dc24a2e8b84b8db
Abdullah Yusuf <abdullahyildirim21@ku.edu.tr>
2024-12-16 19:57:44 +0300

    Archer class and archer png added

---
+++ src/main/java/com/rokue/game/entities/Archer.java
@@ -0,0 +1,49 @@
+package com.rokue.game.entities;
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+import com.rokue.game.map.Hall;
+
+public class Archer extends Entity {
+    private Hall hall;
+    private final Image sprite;
+    private final long attackInterval = 1000;
+    private long lastAttackTime = 0;
+
+    public Archer() {
+        super();
+        sprite = new ImageIcon(getClass().getResource("/sprites/monsters/archer.png")).getImage();
+    }
+
+    public void place(int x, int y, Hall hall) {
+        this.xPosition = x;
+        this.yPosition = y;
+        this.hall = hall;
+    }
+
+    @Override
+    public void update() {
+        long currentTime = System.currentTimeMillis();
+
+        if (hall.getHero() != null) {
+            int heroX = hall.getHero().getXPosition();
+            int heroY = hall.getHero().getYPosition();
+            int distance = Math.abs(heroX - xPosition) + Math.abs(heroY - yPosition);
+
+            if (distance < 4 && currentTime - lastAttackTime > attackInterval) {
+                hall.getHero().decreaseHealth();
+                lastAttackTime = currentTime;
+            }
+        }
+    }
+
+    private void attack(int targetX, int targetY) {
+        System.out.println("Archer attacks at " + targetX + ", " + targetY);
+    }
+
+    @Override
+    public Image getSprite() {
+        return sprite; // Tek görseli döndür
+    }
+}
