d37690edf32ff9dc70c4b3667f0ef283e2bb8e27
toygar321 <tbozkurt21@ku.edu.tr>
2025-01-22 00:51:34 +0300

    WizardBehavior Serializable fix

--- src/main/java/com/rokue/game/entities/WizardBehavior.java
+++ src/main/java/com/rokue/game/entities/WizardBehavior.java
@@ -1,9 +1,11 @@
 package com.rokue.game.entities;
 
+import java.io.Serializable;
+
 import com.rokue.game.map.Hall;
 
-public abstract class WizardBehavior {
-    
+public abstract class WizardBehavior implements Serializable{
+    private static final long serialVersionUID = 1L;
     protected  Hall hall;
     protected Wizard wizard;
 
